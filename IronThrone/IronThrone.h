688a3a0d9302c85ed326e392e69b5331fbeb0f0b
Carl Chen <ccf.developer@gmail.com>
2016-09-18 16:42:57 +0800

    init

---
+++ IronThrone/IronThrone.h
@@ -0,0 +1,19 @@
+//
+//  IronThrone.h
+//  IronThrone
+//
+//  Created by Carl Chen on 9/18/16.
+//  Copyright © 2016 serious. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IronThrone.
+FOUNDATION_EXPORT double IronThroneVersionNumber;
+
+//! Project version string for IronThrone.
+FOUNDATION_EXPORT const unsigned char IronThroneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IronThrone/PublicHeader.h>
+
+
