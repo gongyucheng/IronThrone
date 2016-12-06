b4f4482b8a569dc4ef32ded3b4a81243ef2249f3
Carl Chen <ccf.developer@gmail.com>
2016-12-06 12:30:20 +0800

    add API multipart request

--- Network/Response.swift
+++ Network/Response.swift
@@ -79,3 +79,153 @@ extension HttpRequestable {
 
     }
 }
+
+extension NetworkKit {
+    public static func requestAPIByMultipart(apiHost host: String
+    , apiName: String
+    , multipartParameters: [String: Multipartable]
+    , fileListDic: [String: [Multipartable]] = [:]
+    , headers: [String: String]? = nil
+    , completionHandler: @escaping (HttpResult<Any>) -> Void) {
+        let hostAttributes: HostAttributes? = APIConfiguration.hostsAttributes[host]
+
+        let supportHttps = hostAttributes?.supportHttps ?? true
+        let httpDNSIP: String?
+        if let httpDNSType = hostAttributes?.httpDNSType, case let .httpDNS(ip) = httpDNSType {
+            httpDNSIP = ip
+        } else {
+            httpDNSIP = nil
+        }
+
+        let port = hostAttributes?.port ?? 80
+
+        let urlString = (supportHttps ? "https://" : "http://")
+            + (httpDNSIP == nil ? host : httpDNSIP! )
+            + (port == 80 ? "" : ":\(port)")
+            + "/"
+            + apiName
+
+        var finalHeaders = APIConfiguration.generalHttpHeader
+        if httpDNSIP != nil {
+            finalHeaders["HOST"] = host
+        }
+
+        // handle http header of trace ID.
+        if let traceIDFixedPart = APIConfiguration.traceIDFixedPart, !traceIDFixedPart.isEmpty {
+
+            let timestamp = Int64(Date().timeIntervalSince1970 * 1000)
+            let randomNum: Int64 = timestamp * 10000 + Int64(APIRequest.requestSequence % 10000)
+
+            finalHeaders["x-ricebook-trace"] = traceIDFixedPart + "-" + randomNum.irt.base36String
+
+            APIRequest.requestSequence += 1
+            if APIRequest.requestSequence >= 10000 {
+                APIRequest.requestSequence %= 10000
+            }
+        }
+
+
+        let group = APIRequest.networkGroup
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        group.enter()
+
+        shared.alamofireManager
+            .upload(multipartFormData: { (multipartFormData) in
+                for (key, value) in multipartParameters {
+                    let packetAttribute = value.packetAttribute()
+                    if let fileName = packetAttribute.fileName {
+                        if let mimeType = packetAttribute.mimeType {
+                            multipartFormData.append(packetAttribute.data
+                                , withName: key, fileName: fileName, mimeType: mimeType)
+                        }
+                    }else if let mimeType = packetAttribute.mimeType {
+                        multipartFormData.append(packetAttribute.data
+                            , withName: key, mimeType: mimeType)
+                    }else {
+                        multipartFormData.append(packetAttribute.data, withName: key)
+                    }
+
+                }
+
+                for (key, value) in fileListDic {
+                    for (index, item) in value.enumerated() {
+                        let packetAttribute = item.packetAttribute()
+                        let fileName = packetAttribute.fileName ?? "\(index)"
+                        let mimeType = packetAttribute.mimeType ?? "text/plain"
+                        multipartFormData.append(packetAttribute.data, withName: key, fileName: fileName, mimeType: mimeType)
+                    }
+                }
+            }, to: urlString, headers: finalHeaders) { (encodingResult) in
+                switch encodingResult {
+                case .success(let upload, _ , _):
+                    upload.responseJSON { (response) in
+                        group.leave()
+
+                        let result: HttpResult<Any>
+                        switch response.result {
+                        case let .success(json):
+                            result = HttpResult.success(json)
+                        case let .failure(error):
+                            if let serverErrorInfo = response.data?.irt.toJsonObject() as? [String: Any]
+                            , let errorCode = serverErrorInfo["error_code"] as? Int {
+                                let displayMsg = serverErrorInfo["display_msg"] as? String
+                                let serverError = NetworkError.APIServerError(code: errorCode,
+                                                                              displayMsg: displayMsg)
+                                result = HttpResult.failure(serverError)
+                            } else {
+                                result = HttpResult.failure(error)
+                            }
+                        }
+
+                        completionHandler(result)
+
+                    }
+                case .failure(_):
+                    group.leave()
+
+                    let error = HttpResult<Any>.failure(NetworkError.multipartDataEncodingIncorrect)
+                    completionHandler(error)
+                }
+            }
+
+
+        group.notify(queue: DispatchQueue.main) {
+            UIApplication.shared.isNetworkActivityIndicatorVisible = false
+        }
+
+
+    }
+}
+
+public struct MultipartDataPacketAttribute {
+    var data: Data
+    var mimeType: String?
+    var fileName: String?
+
+    init(data: Data) {
+        self.data = data
+    }
+}
+
+public protocol Multipartable {
+    func packetAttribute() -> MultipartDataPacketAttribute
+}
+
+extension UIImage: Multipartable {
+
+    public func packetAttribute() -> MultipartDataPacketAttribute {
+        var result = MultipartDataPacketAttribute(data: UIImageJPEGRepresentation(self, 0.8)!)
+        result.mimeType = "image/jpeg"
+
+        return result
+    }
+
+}
+
+extension String: Multipartable {
+
+    public func packetAttribute() -> MultipartDataPacketAttribute {
+        return MultipartDataPacketAttribute(data: self.data(using: String.Encoding.utf8)!)
+    }
+
+}
